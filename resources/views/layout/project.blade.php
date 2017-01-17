89372063e584a4b376c11555a7ad376744b3aa41
Muhammad Nurul Asiqi <muhammad.asiqi08@gmail.com>
2017-01-17 11:12:10 +0700

    layout

---
+++ resources/views/layout/project.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>prot</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top navbar-cp">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" title="Thunderlab"><img src="image/web.jpg" class="logo-img" alt="Thunderlab"></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav center">
+                <li ><a href="#">Labolatory</a></li>
+                <li><span class="sprite ie"></span></li>
+                <li><a href="#">Portofolio</a></li>
+            </ul>
+
+        </div>
+    </div>
+
+</nav>
+<section>
+<img style="width:inherit;height: 700px" src="image/hero.jpg">
+</section>
+<section id="/" class="content-section text-center">
+	<div class="row">
+	<h1> TECH SOLUTION ARTISAN </h1>
+	<p style="font-size: 14pt;"><br>In the thunderweb,we crafted thousands line of beautiful codes
+	<br>into valuable system
+	<br>we kept squad focus on their best</p>
+	</div>
+	<div class="row">
+  <div class="col-sm-4"><h1>WEB DESIGN</h1><p style="font-size: 12pt;"><br>Interface Design
+  <br>Responsive Design <br>UX Srategy</p></div>
+  <div class="col-sm-4"><h1>WEB DEVELOPMENT</h1><p style="font-size: 12pt;"><br>System Analyst
+  <br>Bussines Flow & Policy<br>Softwere Architecture</p></div>
+  <div class="col-sm-4"><h1>IMPLIMENTATION</h1><P style="font-size: 12pt";><br>System Performance
+  <br>Technical Help<br>QA & User Testing</P></div>
+</div>
+</section>
+<sectionid="/" class="content-section text-center">
+	<div>
+		<h1>YOUR TIME TOO PRECIOUS</h1>
+		<p style="font-size: 14pt";><br>We are focusing in helping small business to run like a
+		<br>big cooperate,so that they can focus on their growth.We<br>are archieving that by offering
+		tech solution with many<br>service like accounting tech solution,data analysis<br>solution,HR
+		tech solution,costumer relation management,<br>management tech solution, and much more.
+	</div>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
